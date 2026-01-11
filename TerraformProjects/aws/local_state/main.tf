429a0329249d9ea0568a9251034c9d87c577416d
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2026-01-11 17:15:50 +0530

    Update AWS provider region and instance configuration

--- TerraformProjects/aws/local_state/main.tf
+++ TerraformProjects/aws/local_state/main.tf
@@ -1,23 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region  = "us-west-2"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = "ami-0ced6a024bb18ff2e"
+  instance_type = "t3.micro"
 
-  tags = {
-    Name = "Terraform_Demo"
+ tags = {
+    Name = "AppServer"
   }
 }
